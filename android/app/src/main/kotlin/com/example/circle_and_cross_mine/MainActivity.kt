b52b316d1f6c3ec24fa240fc36f770d73c702da3
bhn0108 <bhn0108@icloud.com>
2021-05-24 12:39:54 +0900

    first commit

---
+++ android/app/src/main/kotlin/com/example/circle_and_cross_mine/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.circle_and_cross_mine
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+
+
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(FontAwesomeIcons.circle, color: Colors.green, size: 30,),
+            Icon(Icons.clear, color: Colors.red, size:30,),
+            Text('ゲーム')
+          ],
+        ),
+      ),
+      body: Center(
+
+        child: Column(
+
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+//todo アプリのタイトル変更
+//todo フィールドのUIを作成
+//todo フィールドのUI作成をメソッドを用いて簡潔に
+//todo ターン表示とクリアボタンの作成
+//todo マス目をタップ可能にし、タップ時にターン切り替え
+//todo マス目タップでマルバツを表示
+//todo ゲームの勝者のパターンを書き出す
+//todo ゲームの勝敗を判定可能に
+//todo リセットボタンタップでリスタート可能に
